f098421dd5f10213a2c8132be02eb0ef6ae23744
Juan Chemello <juanchemell@gmail.com>
2020-10-03 21:45:16 +0000

    Project initialized

---
+++ server.js
@@ -0,0 +1,35 @@
+//? set env variables
+const dotEnv = require('dotenv')
+dotEnv.config()
+
+//? imports
+const cors = require('cors')
+const express = require('express')
+
+//? express
+const app = express()
+const PORT = process.env.PORT
+
+//? middlewares
+app.use(cors())
+app.use(express.json())
+app.use((err, _req, res, next) => {
+  console.log(err.message)
+  if (err)
+    return res.status(err.status).json({
+      error: 'Bad request. Usually means a malformed JSON, check the syntax.'
+    })
+  next()
+})
+
+//? database connection
+const db = require('./config/database')
+db.authenticate()
+  .then(() => console.log('DB connected.'))
+  .then(() =>
+    app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+  )
+  .catch(error => console.log(error.message))
+
+//Routes routes
+app.get('/', (_req, res) => res.send('INDEX'))
